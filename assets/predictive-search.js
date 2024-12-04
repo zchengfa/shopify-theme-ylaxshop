6ee6b03edba267cbba177ecfe5f0c6815656f42e
zchengfa <1522977814@qq.com>
2024-12-04 18:48:12 +0800

    feat(顶部搜索框添加搜索建议功能):

---
+++ assets/predictive-search.js
@@ -0,0 +1,80 @@
+
+  
+  if(!customElements.get('predictive-search')) {
+    customElements.define('predictive-search', class PredictiveSearch extends HTMLElement {
+      constructor() {
+        super();
+    
+        this.input = this.querySelector('input[type="search"]');
+        this.predictiveSearchResults = this.querySelector('#predictive-search-results-box');
+        
+        this.input.addEventListener('input', this.debounce((event) => {
+          this.onChange(event);
+        }, 300).bind(this));
+      }
+    
+      onChange() {
+        const searchTerm = this.input.value.trim();
+    
+        if (!searchTerm.length) {
+          this.close();
+          return;
+        }
+    
+        this.getSearchResults(searchTerm);
+      }
+    
+      getSearchResults(searchTerm) {
+        fetch(`search/suggest?q=${searchTerm}&section_id=predictive-search`)
+          .then((response) => {
+            if (!response.ok) {
+              var error = new Error(response.status);
+              this.close();
+              throw error;
+            }
+    
+            return response.text();
+          })
+          .then((text) => {
+            const resultsMarkup = new DOMParser().parseFromString(text, 'text/html').querySelector('#shopify-section-predictive-search');
+            const spanArr = resultsMarkup.querySelectorAll('span');
+           
+            spanArr.forEach((span) => {
+              let spanText = span.innerText;
+              span.classList.add('predictive-search-product-text');
+              //替换特定字符串
+              spanText = spanText.replace(spanText.match(new RegExp(searchTerm, 'i')), `<span class="predictive-search__result--highlight">${spanText.match(new RegExp(searchTerm, 'i'))}</span>`);
+              span.innerHTML = spanText;
+            })
+            this.predictiveSearchResults.innerHTML = resultsMarkup.innerHTML;
+            this.open();
+          })
+          .catch((error) => {
+            this.close();
+            throw error;
+          });
+      }
+      //首字母大写
+      capitalizeFirstLetter(string) {
+        return string.toLowerCase().replace(/\b[a-z]/g, function(match) {
+          return match.toUpperCase();
+        });
+      }
+    
+      open() {
+        this.predictiveSearchResults.style.display = 'block';
+      }
+    
+      close() {
+        this.predictiveSearchResults.style.display = 'none';
+      }
+    
+      debounce(fn, wait) {
+        let t;
+        return (...args) => {
+          clearTimeout(t);
+          t = setTimeout(() => fn.apply(this, args), wait);
+        };
+      }
+    });
+  }
